e243ac5762d81235151d1432fd4876009dfd0333
Christophe Knage <cvknage@gmail.com>
2016-09-25 12:50:21 +0200

    Travis CI

---
+++ deploy-ghpages.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Deployment script that pushes to gh-pages
+# From https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+
+set -e # exit with nonzero exit code if anything fails
+
+# clean and build our app
+gulp clean
+gulp dist
+
+# go to the out directory and create a *new* Git repo
+cd target/dist
+git init
+#git config --global url."https://".insteadOf git://﻿
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "<you>@<your-email>"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "New deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
